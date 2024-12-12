9b397374399a8d22b51bd13e58911e5b076a92fb
Ellen J <jcrescent61@gmail.com>
2024-12-12 13:37:50 +0900

    feat: 예외처리 추가
    - IndexOutOfBoundsException
    - NumberFormatException

--- src/Hyeji/Lotto/Main.java
+++ src/Hyeji/Lotto/Main.java
@@ -14,8 +14,15 @@ public class Main {
 
     public static void startProgram() {
         setGames();
-        Gameable selectedGame = games.get(scanner.nextInt() - 1);
-        selectedGame.startGame();
+        try {
+            int selectedGameIndex = (scanner.nextInt() - 1);
+            Gameable selectedGame = games.get(selectedGameIndex - 1);
+            selectedGame.startGame();
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("잘못된 번호를 선택했습니다. 프로그램을 종료합니다.");
+        } catch (NumberFormatException e) {
+            System.out.println("숫자를 입력해주세요. 프로그램을 종료합니다.");
+        }
     }
 
     private static void setGames() {
